1473648c281b80c39b896e2788e57bcee3d90759
keirbailey <keirinvestment@gmail.com>
2021-01-27 18:36:19 +0000

    Added first service, routes and handlers

--- api/routes.go
+++ api/routes.go
@@ -1,1 +1,17 @@
 package api
+
+import "github.com/go-chi/chi/middleware"
+
+func (s *server) registerRoutes() {
+	// Middleware
+	s.router.Use(middleware.Logger)
+	// Todo: Add a sub route
+
+	// Command
+
+	// Query
+	s.router.Get("/", s.)
+
+
+
+}
--- api/server.go
+++ api/server.go
@@ -1,13 +1,60 @@
 package api
 
 import (
+	"fmt"
 	"github.com/go-chi/chi"
+	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/service"
+	"go.uber.org/zap"
 	"net/http"
 )
 
+// server is a HTTP server.
 type server struct {
-	httpServer http.Server
-	service service.Service
+	http.Server
 	router *chi.Mux
 	logger *zap.Logger
+	fmService service.Service
+	name string
+	version string
+}
+
+// config is the HTTP server configuration.
+type config struct {
+	// logger is the logging instance to use
+	logger *zap.Logger
+	// fmService is the financial modelling service
+	fmService fmService
+	// name is the name of the service
+	name string
+	// version is the version of the service
+	version string
+	// port is the HTTP port to serve on
+	port int
+}
+
+// NewServer creates a new server.
+func NewServer(cfg *config) *server {
+	s := &server{
+		logger: cfg.logger,
+		fmService: cfg.fmService,
+		name: cfg.name,
+		version: cfg.version,
+	}
+	s.router = chi.NewRouter()
+	s.Server = http.Server{
+		Addr: fmt.Sprintf(":%d", cfg.port),
+		Handler: s,
+	}
+	s.re
+}
+
+func (s *server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.router.ServeHTTP(w, r)
+}
+
+func (s *server) Run() {
+	if err := s.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+		s.logger.Error(err.Error())
+	}
+	// Todo: Handle graceful shutdown with channel pattern!
 }
