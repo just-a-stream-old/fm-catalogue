776d48b8af41491af4bff42117a43746e10ef5f1
keirbailey <keirinvestment@gmail.com>
2021-01-30 19:25:22 +0000

    Added getExchanges() handler func

--- api/handlers.go
+++ api/handlers.go
@@ -1,9 +1,32 @@
 package api
 
-import "net/http"
+import (
+	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/model"
+	"net/http"
+)
 
-func (s *server) listExchanges() http.HandlerFunc {
+func (s *server) getExchanges() http.HandlerFunc {
+	type queryParams struct {
+
+	}
+
+	type response struct {
+		Exchanges []model.Exchange
+	}
+
 	return func(w http.ResponseWriter, r *http.Request) {
-		_, _ = w.Write([]byte("Welcome"))
+		ctx := r.Context()
+
+		exchanges, _, err := s.fMService.GetExchanges(ctx)
+		if err != nil {
+			s.logger.Error(err.Error())
+			w.WriteHeader(http.StatusInternalServerError)
+		}
+
+		response := response{
+			Exchanges: exchanges,
+		}
+
+		s.respondJSON(w, http.StatusOK, response)
 	}
 }
