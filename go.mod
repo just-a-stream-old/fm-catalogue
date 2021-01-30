f32e636dff27594a45a8ca98df3910ddb36924f5
keirbailey <keirinvestment@gmail.com>
2021-01-30 17:30:25 +0000

    Finished creation of dbClient with associated error handling

--- go.mod
+++ go.mod
@@ -6,5 +6,6 @@ require (
 	github.com/go-chi/chi v1.5.1
 	github.com/joho/godotenv v1.3.0
 	github.com/kelseyhightower/envconfig v1.4.0
+	go.mongodb.org/mongo-driver v1.4.5
 	go.uber.org/zap v1.16.0
 )
--- main.go
+++ main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/api"
 	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/config"
 	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/repository"
 	"gitlab.com/open-source-keir/financial-modelling/fm-catalogue/service"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.uber.org/zap"
+	"time"
 )
 
 func main() {
@@ -27,14 +32,18 @@ func run() error {
 		return err
 	}
 
-	//db, err := setupDB()
-	//if err != nil {
-	//	logger.Error(err.Error())
-	//	return err
-	//}
-	//defer closeDB()
+	dbClient, err := setupDB(&cfg.Repository)
+	if err != nil {
+		logger.Error(err.Error())
+		return err
+	}
+	defer func() {
+		if err := closeDB(dbClient); err != nil {
+			logger.Error(err.Error())
+		}
+	}()
 
-	fMRepository, err := repository.NewFMRepository(&cfg.Repository, logger)
+	fMRepository, err := repository.NewFMRepository(&cfg.Repository, logger, dbClient)
 	if err != nil {
 		logger.Error(err.Error())
 		return err
@@ -42,9 +51,30 @@ func run() error {
 
 	fMService := service.NewFMService(&cfg.Service, logger, fMRepository)
 
-	server := api.NewServer(&cfg.Server, logger, fMService)
+	server, err := api.NewServer(&cfg.Server, logger, fMService)
+	if err != nil {
+		logger.Error(err.Error())
+		return err
+	}
 
 	server.Run()
 
 	return nil
 }
+
+func setupDB(cfg *config.Repository) (*mongo.Client, error) {
+	// Cancel if database connection is not established within time provided
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	connStr := fmt.Sprintf("%s://%s:%s", cfg.DBDriver, cfg.DBHost, cfg.DBPort)
+	dbClient, err := mongo.Connect(ctx, options.Client().ApplyURI(connStr))
+
+	return dbClient, err
+}
+
+func closeDB(dbClient *mongo.Client) error {
+	return dbClient.Disconnect(context.TODO())
+}
+
+
