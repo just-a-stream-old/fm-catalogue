ebdebe6f565047a5c4b872396ffde4a5be0e59b6
keirbailey <keirinvestment@gmail.com>
2021-01-30 19:24:39 +0000

    Updated Exchange struct to be accurate

--- model/model.go
+++ model/model.go
@@ -1,1 +1,10 @@
 package model
+
+type Exchange struct {
+	ExchangeCode 	string		`json:"exchangeCode" bson:"exchangeCode"`
+	Name 			string		`json:"name" bson:"name"`
+	MarketIdCode	string		`json:"marketIdCode" bson:"marketIdCode"`
+	Country 		string		`json:"country" bson:"country"`
+	Currency 		string		`json:"currency" bson:"currency"`
+	FinanceAPIs		[]string	`json:"financeApis" bson:"financeApis"`
+}
